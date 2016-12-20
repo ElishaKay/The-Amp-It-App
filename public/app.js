022489f9aa364496e161b5fd7436016429430919
Elisha <kramer1346@gmail.com>
2016-12-20 21:01:35 +0200

    Add main project files

---
+++ public/app.js
@@ -0,0 +1,31 @@
+var app = angular.module('Amp', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/home');
+  
+  // First Page
+  $stateProvider.state('home', {
+    url: '/home',
+    templateUrl: 'partial-home.html'
+  })
+  .state('home.list', {
+  	url: '/list',
+  	templateUrl: 'home-list.html',
+  	controller: function($scope) {
+      $scope.scotches = ['Balvenie', 'The Balmore', 'Highland Park'];
+    }
+  }).state('about', {
+    url: '/about',
+    templateUrl: 'About-us.html'
+  }).
+    state('scotch', {
+    url: '/scotch/:scotch',
+    templateUrl: function ($stateParams){
+      console.log($stateParams)
+      return 'partial-scotch-' + $stateParams.scotch + '.html';
+    }
+  })
+
+
+});
